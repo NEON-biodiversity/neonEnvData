e07ae16c3e3ef13063af3c0dc1471ded2003217c
kellykapsar <kelly.kapsar@gmail.com>
2024-10-29 16:07:52 -0500

    Create separate script for conceptual figure generation.

---
+++ R/generate_scale_figure.R
@@ -0,0 +1,123 @@
+# Load necessary libraries
+library(sf)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+source("./R/config.R")
+
+# Load data into memory
+site <- st_read(paste0(data_dir, "NEON_spatial/EPSG5070/NEON_sites.shp"), quiet = TRUE)
+plt <- st_read(paste0(data_dir, "NEON_spatial/EPSG5070/NEON_small_mammal_plots.shp"), quiet=TRUE)
+plt_radii <- st_read(paste0(data_dir, "NEON_spatial/EPSG5070/plot_radii.shp"), quiet = TRUE)
+plt_circle_center <- st_read(paste0(data_dir, "NEON_spatial/EPSG5070/plot_circle_centers.shp"), quiet = TRUE)
+site_radii <- st_read(paste0(data_dir, "NEON_spatial/EPSG5070/site_radii.shp"), quiet = TRUE)
+site_circle_center <- st_read(paste0(data_dir, "NEON_spatial/EPSG5070/site_circle_centers.shp"), quiet = TRUE)
+dom_radii <- st_read(paste0(data_dir, "NEON_spatial/EPSG5070/domain_radii.shp"), quiet = TRUE)
+dom <- st_read(paste0(data_dir, "NEON_spatial/EPSG5070/NEON_domains.shp"), quiet = TRUE)
+
+# Refactor function
+refactor_code <- function(site_code, site, plt, plt_radii, plt_circle_center, site_radii, site_circle_center, dom_radii, dom) {
+  # Subset data based on the input site_code
+  site_data <- site[site$siteID == site_code,]
+  traps_data <- plt[grep(site_code, plt$plotID),]
+  plot_radii_data <- plt_radii[plt_radii$siteID == site_code,]
+  focal_plot_data <- plt_circle_center[plt_circle_center$plotID == traps_data$plotID[1],]
+  plots_data <- plt_circle_center[plt_circle_center$siteID == site_code,]
+  
+  radii_data <- site_radii[site_radii$siteID == site_code,]
+  circle_center_data <- site_circle_center[site_radii$siteID == site_code,]
+  dom_radii_data <- dom_radii[dom_radii$siteID == site_code,]
+  dom_data <- dom[dom$domainNumb == site_data$domainNumb,]
+  
+  # First plot
+  fig_plot <- ggplot() +
+    geom_sf(data = plot_radii_data[plot_radii_data$plotID == traps_data$plotID[1],], fill = "#b8e6a5") +
+    geom_sf(data = traps_data[traps_data$plotID == traps_data$plotID[1], ], color = "black", size = 3) +
+    theme_minimal() +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title = element_blank(),
+      plot.title = element_blank()
+    )
+  
+  ggsave(filename = paste0("./figures/", site_code, "_plot.png"), plot = fig_plot, width = 8, height = 6, dpi = 300)
+  
+  # Second plot
+  fig_site_footprint <- ggplot() +
+    geom_sf(data = site_data, fill = "#44aea3") +
+    geom_sf(data = plots_data, shape = 21, fill = "#b8e6a5", color = "black", size = 7) +
+    theme_minimal() +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title = element_blank(),
+      plot.title = element_blank()
+    )
+  
+  ggsave(filename = paste0("./figures/", site_code, "_site_footprint.png"), plot = fig_site_footprint, width = 8, height = 6, dpi = 300)
+  
+  # Third plot
+  fig_site_radii <- ggplot() +
+    geom_sf(data = radii_data, fill = "#44aea3") +
+    geom_sf(data = site_data, fill = NA, color = "white", size = 0.7) +
+    geom_sf(data = plots_data, shape = 21, fill = "#b8e6a5", color = "black", size = 3) +
+    theme_minimal() +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title = element_blank(),
+      plot.title = element_blank()
+    )
+  
+  ggsave(filename = paste0("./figures/", site_code, "_site_radius.png"), plot = fig_site_radii, width = 8, height = 6, dpi = 300)
+  
+  # Fourth plot (dom_radii)
+  buffer_extent <- st_buffer(dom_radii_data, dist = 100000)
+  
+  fig_dom_radii <- ggplot() +
+    geom_sf(data = dom_data, color = "black", alpha = 0.5) +
+    geom_sf(data = dom_radii_data, fill = "#05718b") +
+    geom_sf(data = radii_data, fill = "#44aea3", color = "black", size = 0.7) +
+    coord_sf(xlim = st_bbox(buffer_extent)[c("xmin", "xmax")],
+             ylim = st_bbox(buffer_extent)[c("ymin", "ymax")]) +
+    theme_minimal() +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title = element_blank(),
+      plot.title = element_blank()
+    )
+  
+  ggsave(filename = paste0("./figures/", site_code, "_dom_radius.png"), plot = fig_dom_radii, width = 8, height = 6, dpi = 300)
+  
+  # Fifth plot (dom_footprint)
+  fig_dom_footprint <- ggplot() +
+    geom_sf(data = dom_data, fill = "#05718b") +
+    geom_sf(data = site_data, fill = "#44aea3", color = "#44aea3", size = 0.7, alpha = 0.5) +
+    theme_minimal() +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title = element_blank(),
+      plot.title = element_blank()
+    )
+  
+  ggsave(filename = paste0("./figures/", site_code, "_dom_footprint.png"), plot = fig_dom_footprint, width = 8, height = 6, dpi = 300)
+}
+
+# Example usage:
+lapply(unique(site$siteID), function(x){
+  refactor_code(x, site, plt, plt_radii, plt_circle_center, site_radii, site_circle_center, dom_radii, dom)
+})
