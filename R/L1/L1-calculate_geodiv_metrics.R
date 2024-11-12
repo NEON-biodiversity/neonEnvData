9ebaa53f93bbf615fb284290ac2d7b3760bd4728
kellykapsar <kelly.kapsar@gmail.com>
2024-11-12 17:08:47 -0500

    Generate script to calculate geodiversity metrics.

---
+++ R/L1/L1-calculate_geodiv_metrics.R
@@ -0,0 +1,12 @@
+# TITLE:            Geodiversity Metric Calculations
+# PROJECT:          NEON Geodiversity Analysis
+# AUTHORS:          Kelly Kapsar, Pat Bills, Phoebe Zarnetske 
+# COLLABORATORS:    Lala Kounta
+# DATA INPUT:       SRTMGl3_v003 data downloaded from NASA EarthData 
+# DATA OUTPUT:       
+# DATE:             November 2024
+# OVERVIEW:          
+# REQUIRES:         
+# NOTES:       
+
+
