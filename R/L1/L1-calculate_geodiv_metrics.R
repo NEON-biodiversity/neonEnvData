78b3c013cde955e5f6aba26712860028029e0a59
kellykapsar <kelly.kapsar@gmail.com>
2024-11-18 14:33:09 -0500

    Functional extraction methods using (1) tile map and (2) duckdb. Both work, but going to cut out duckdb method for sake of reproducibility.

--- R/L1/L1-calculate_geodiv_metrics.R
+++ R/L1/L1-calculate_geodiv_metrics.R
@@ -15,30 +15,57 @@ library(terra)
 library(sf)
 library(dplyr)
 
-spat_data <- list.files("/mnt/scratch/plz-lab/geodiversity/spatial_data/polys_EPSG5070/") %>% grep("\\.shp$", ., value=TRUE)
-tiles <- 
+source("./R/config.R")
 
+spat_data <- list.files("/mnt/scratch/plz-lab/geodiversity/spatial_data/polys_EPSG5070/", full.names=T) %>% grep("\\.shp$", ., value=TRUE)
+
+srtm_tiles <- st_read("/mnt/scratch/plz-lab/geodiversity/spatial_data/SRTM_tiles/srtm_grid_1deg.shp") %>% st_transform(prj)
+
+################################################################################
+###### Method using raw hgt files and cropping 
+start_time <- Sys.time()
+
+cutters <- st_read(spat_data[3])
+cutter <- cutters[cutters$siteID == "ABBY",]
+
+test <- srtm_tiles[st_intersects(cutter, srtm_tiles, sparse=F),]
+
+tiles <- list.files("/mnt/scratch/plz-lab/geodiversity/SRTM_gl1_v003/tiles_EPSG5070", full.names=TRUE) %>%
+  grep( paste(unique(test$id), collapse = "|"), ., value = TRUE)
+
+# temp <- lapply(tiles, function(x){unzip(x, exdir = "/mnt/scratch/plz-lab/geodiversity/SRTM_gl1_v003/unzipped_hgt") %>% rast})
+temp <- lapply(tiles, rast)
+
+if(length(temp) > 1){
+  ras1 <- do.call(terra::mosaic, temp) %>% terra::crop(., st_bbox(cutter) %>% terra::mask(cutter))
+}else(ras1 <- temp[[1]] %>% terra::crop(., st_bbox(cutter)) %>% terra::mask(cutter))
+
+plot(ras1)
+
+end_time <- Sys.time() - start_time
+print(end_time)
+
+################################################################################
+###### Method using output from duckdb 
+start_time2 <- Sys.time()
 
 ls <- list.files("/mnt/scratch/plz-lab/geodiversity/output", full.names=TRUE) %>%
   grep("intersected", ., value = TRUE)
 df <- read.csv(ls[23])
 
 # Isolate one spatial unit
-d <- df[df$siteID == "DSNY",]
+d <- df[df$siteID == "ABBY",]
 
 # Convert csv back to raster
-coordinates <- vect(d, geom = c("x", "y"), crs = "EPSG:4326") 
+coordinates <- vect(d, geom = c("x", "y"), crs = "EPSG:4326") %>% terra::project("EPSG:5070")
 
-tiles <- list.files("/mnt/scratch/plz-lab/geodiversity/SRTM_gl1_v003", full.names=TRUE) %>% 
-  grep( paste(unique(d$tile_id), collapse = "|"), ., value = TRUE) %>% 
-  grep("\\.zip$", ., value = TRUE)
-  
-temp <- lapply(tiles, function(x){unzip(x, exdir = "/mnt/scratch/plz-lab/geodiversity/SRTM_gl1_v003/unzipped_hgt") %>% rast})
+r_template <- rast(ext=ext(coordinates), resolution = 30, crs = "EPSG:5070")
+ras2 <- rasterize(coordinates, r_template, field = "elevation")
+plot(ras2)
 
-t <- do.call(terra::mosaic, temp) %>% terra::crop(., st_bbox(coordinates))
-
-ras <- rasterize(coordinates, t, field = "elevation")
-
+end_time2 <- Sys.time() - start_time2
+print(end_time2)
+################################################################################
 
 mets <- c("sq", "sdq", "sbi", "ssk", "sku", "sfd", "sds", "std")
 # NOTE stdi is the actual metric we're using, but it comes out of calling std in the function call 
