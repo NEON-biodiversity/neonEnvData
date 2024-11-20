2b2bf669c6b2345ce4859c193bea61f2610ba754
kellykapsar <kelly.kapsar@gmail.com>
2024-11-20 12:53:37 -0500

    Generate geodiv wrapper function to calculate metric list on all lines in a shapefile individually.

--- R/L1/L1-calculate_geodiv_metrics.R
+++ R/L1/L1-calculate_geodiv_metrics.R
@@ -9,78 +9,96 @@
 # REQUIRES:         
 # NOTES:       
 
-
 library(geodiv)
 library(terra)
 library(sf)
 library(dplyr)
 
-source("./R/config.R")
+calculate_geodiv_metrics <- function(srtm_tiles, srtm_tile_files, spatial_polys, metrics_list) {
+  # Loop through each polygon
+  for (i in 1:nrow(spatial_polys)) {
+    cutter <- spatial_polys[i, ]
+    
+    print(paste0("Processing polygon ", i, " of ", nrow(spatial_polys), "."))
+    
+    # Find intersecting SRTM tiles
+    intersecting_tiles <- srtm_tiles[st_intersects(cutter, srtm_tiles, sparse = FALSE), ]
+    
+    if(length(intersecting_tiles$id) == 0){
+      metrics_values <- rep(NA, length(metrics_list))
+    }else{
+      # Get the list of relevant SRTM tile files
+      tiles <- srtm_tile_files %>%
+        grep(paste(unique(intersecting_tiles$id), collapse = "|"), ., value = TRUE)
+      
+      # Load and mosaic the relevant SRTM tiles
+      temp <- lapply(tiles, rast)
+      
+      if (length(temp) > 1) {
+        ras1 <- do.call(terra::mosaic, temp) %>% terra::crop(., st_bbox(cutter)) %>% terra::mask(cutter)
+      } else {
+        ras1 <- temp[[1]] %>% terra::crop(., st_bbox(cutter)) %>% terra::mask(cutter)
+      }
+      
+      # Calculate metrics for the polygon
+      metrics_values <- sapply(metrics_list, function(metric) {
+        if (metric == "std") {
+          stop("Error: Please specify 'std1' or 'std2' as the metric name.")
+        }
+        if (metric == "std1") {
+          return(tryCatch({
+            metric_function <- get("std", envir = asNamespace("geodiv"))
+            as.numeric(metric_function(ras1)[1])
+          }, error = function(e) {
+            message(sprintf("Geodiv metric '%s' does not exist.", metric))
+            NA
+          }))
+        }
+        if (metric == "std2") {
+          return(tryCatch({
+            metric_function <- get("std", envir = asNamespace("geodiv"))
+            as.numeric(metric_function(ras1)[2])
+          }, error = function(e) {
+            message(sprintf("Geodiv metric '%s' does not exist.", metric))
+            NA
+          }))
+        }
+        tryCatch({
+          metric_function <- get(metric, envir = asNamespace("geodiv"))
+          as.numeric(metric_function(ras1))
+        }, error = function(e) {
+          message(sprintf("Geodiv metric '%s' does not exist.", metric))
+          NA
+        })
+      })
+  }
+  print(metrics_values)
 
-spat_data <- list.files("/mnt/scratch/plz-lab/geodiversity/spatial_data/polys_EPSG5070/", full.names=T) %>% grep("\\.shp$", ., value=TRUE)
+  # Add the metric values as new columns to the spatial_polys object
+  for (j in seq_along(metrics_list)) {
+    spatial_polys[[metrics_list[j]]][i] <- as.numeric(metrics_values[j])
+  }
+  rm(metrics_values)
+}
+return(spatial_polys)
+}
 
-srtm_tiles <- st_read("/mnt/scratch/plz-lab/geodiversity/spatial_data/SRTM_tiles/srtm_grid_1deg.shp") %>% st_transform(prj)
+# Example usage (load shapefiles externally)
+srtm_tiles <- st_read("/mnt/scratch/plz-lab/geodiversity/spatial_data/SRTM_tiles/srtm_grid_1deg.shp") %>% st_transform(crs = 5070)
+srtm_tile_files <- list.files("/mnt/scratch/plz-lab/geodiversity/SRTM_gl1_v003/tiles_EPSG5070", full.names = TRUE)
+spatial_polys <- lapply(grep(".shp", list.files("/mnt/scratch/plz-lab/geodiversity/spatial_data/polys_EPSG5070/", full.names = T), value=T), st_read)
+metrics_list <- c("sq", "sdq", "sbi", "ssk", "sku", "sfd", "sds", "std2")
 
-################################################################################
-###### Method using raw hgt files and cropping 
-start_time <- Sys.time()
-
-cutters <- st_read(spat_data[3])
-cutter <- cutters[cutters$siteID == "ABBY",]
-
-test <- srtm_tiles[st_intersects(cutter, srtm_tiles, sparse=F),]
-
-tiles <- list.files("/mnt/scratch/plz-lab/geodiversity/SRTM_gl1_v003/tiles_EPSG5070", full.names=TRUE) %>%
-  grep( paste(unique(test$id), collapse = "|"), ., value = TRUE)
-
-# temp <- lapply(tiles, function(x){unzip(x, exdir = "/mnt/scratch/plz-lab/geodiversity/SRTM_gl1_v003/unzipped_hgt") %>% rast})
-temp <- lapply(tiles, rast)
-
-if(length(temp) > 1){
-  ras1 <- do.call(terra::mosaic, temp) %>% terra::crop(., st_bbox(cutter) %>% terra::mask(cutter))
-}else(ras1 <- temp[[1]] %>% terra::crop(., st_bbox(cutter)) %>% terra::mask(cutter))
-
-plot(ras1)
-
-end_time <- Sys.time() - start_time
-print(end_time)
-
-################################################################################
-###### Method using output from duckdb 
-start_time2 <- Sys.time()
-
-ls <- list.files("/mnt/scratch/plz-lab/geodiversity/output", full.names=TRUE) %>%
-  grep("intersected", ., value = TRUE)
-df <- read.csv(ls[23])
-
-# Isolate one spatial unit
-d <- df[df$siteID == "ABBY",]
-
-# Convert csv back to raster
-coordinates <- vect(d, geom = c("x", "y"), crs = "EPSG:4326") %>% terra::project("EPSG:5070")
-
-r_template <- rast(ext=ext(coordinates), resolution = 30, crs = "EPSG:5070")
-ras2 <- rasterize(coordinates, r_template, field = "elevation")
-plot(ras2)
-
-end_time2 <- Sys.time() - start_time2
-print(end_time2)
-################################################################################
-
-mets <- c("sq", "sdq", "sbi", "ssk", "sku", "sfd", "sds", "std")
-# NOTE stdi is the actual metric we're using, but it comes out of calling std in the function call 
-
-geodiv::sq(test)
-geodiv::sdq(test)
-geodiv::sbi(test)
-geodiv::ssk(test)
-geodiv::sku(test)
-geodiv::sfd(test)
-geodiv::sds(test)
-geodiv::std(test)[2]
-
-win <- matrix(1, nrow = 7, ncol = 7)
-
-geodiv::focal_metrics(test, window = win, metrics = "sq", progress=TRUE)
+# Call function with pre-loaded objects
+# result <- calculate_geodiv_metrics(srtm_tiles, srtm_tile_files, spatial_polys[[6]], metrics_list)
+# result <- calculate_geodiv_metrics(srtm_tiles, srtm_tile_files, spatial_polys[[6]], metrics_list)
+# print(result)
 
 
+for(i in 1:length(spatial_polys)){
+
+  results <- calculate_geodiv_metrics(srtm_tiles, srtm_tile_files, spatial_polys[[i]], metrics_list)
+  st_write(results, paste0("/mnt/scratch/plz-lab/geodiversity/output/polys_EPSG5070_intersected/", 
+                           grep(".shp", list.files("/mnt/scratch/plz-lab/geodiversity/spatial_data/polys_EPSG5070/"), value=T)[i]))
+  print(  grep(".shp", list.files("/mnt/scratch/plz-lab/geodiversity/spatial_data/polys_EPSG5070/"), value=T)[i])
+}
